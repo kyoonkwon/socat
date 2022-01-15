d449c8a8d93126f1cab2fa391503cf4409acf86e
sunwookim <sunkim09@g.skku.edu>
2022-01-16 00:56:21 +0900

    add move

--- skp/src/components/fish_game/FishingRod.js
+++ skp/src/components/fish_game/FishingRod.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import RodImg from './image/RodImg.png'
+import RodImg from './image/m_RodImg.png'
+import styled from 'styled-components'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -8,8 +9,8 @@ const useStyles = makeStyles(theme => ({
     },
     character: {
       position: 'absolute',
-      width:'80px',
-      height:'120px',
+      width:'60px',
+      height:'400px',
       left: '150px',
     },
   }))
@@ -18,19 +19,22 @@ const useStyles = makeStyles(theme => ({
     // init
     const updateTime = 20;
     const initLeft = 50;
+    const initTop = 10;
     const speed = 10;
     const moveSize = 200;
     const [left, setLeft] = useState(initLeft);
+    const [top, setTop] = useState(initTop);
     var isMove = false;
     const timeOutList = [];
     
     const classes = useStyles();
 
     useEffect(() => {
+        document.addEventListener('keydown', handleKeyUp)
+        document.addEventListener('keydown', handleKeyDown)
         document.addEventListener('keydown', handleKeyLeft)
         document.addEventListener('keydown', handleKeyRight)
 
-        console.log(document);
         // error : return 안으로 안들어감
         return () => {
             for(let i=0; i< timeOutList.length; ++i) {
@@ -42,9 +46,30 @@ const useStyles = makeStyles(theme => ({
     })
 
     /* 키보드 이벤트 */
+    // up arrow
+    const handleKeyUp = (e) => {
+        if (e.keyCode === 38) {
+          if(!isMove) {
+            isMove = !isMove;
+            moveUp();
+            console.log("click left")
+          }
+        }
+    }
+
+    // down arrow
+    const handleKeyDown = (e) => {
+        if (e.keyCode === 40) {
+          if(!isMove) {
+            isMove = !isMove;
+            moveDown();
+            console.log("click right")
+          }
+        }
+    } 
+
     // left arrow
     const handleKeyLeft = (e) => {
-        console.log("testA")
         if (e.keyCode === 37) {
           if(!isMove) {
             isMove = !isMove;
@@ -56,7 +81,6 @@ const useStyles = makeStyles(theme => ({
 
     //right arrow
     const handleKeyRight = (e) => {
-        console.log("testB")
         if (e.keyCode === 39) {
           if(!isMove) {
             isMove = !isMove;
@@ -64,63 +88,67 @@ const useStyles = makeStyles(theme => ({
             console.log("click right")
           }
         }
-    } 
-    /*
-    // down arrow
-    const handleKeyDown = (e) => {
-
-        if (e.keyCode === 40) {
-          
-        }
     }
 
-    // up arrow
-    const handleKeyDown = (e) => {
-
-      if (e.keyCode === 38) {
-        if (!isJump){
-          isJump = !isJump;
-          jump();
-        }
-      }
+    const moveUp = () => {
+        setTop(top-5);
     }
-    */
- 
+
+    const moveDown = () => {
+        setTop(top+5);
+    }
+
     const moveRight = () => {
-        for(let i=0; i<2*moveSize/speed + 1; i++){
+
+        setLeft(left+5);
+        /*
+        console.log("Right")
+        
+        for(let i=0; i< 2*moveSize/speed + 1; i++){
+            //console.log("test: "+2*moveSize/speed + 1) // 401
             let timeOut = setTimeout(() => {
+                console.log("test: " + timeOut)
                 if( i < moveSize / speed) {
+                    console.log("a"+i)
                     setLeft(initLeft + speed*i);
                 }
                 else {
+                    console.log("b"+i)
                     setLeft(initLeft + speed*(2 * moveSize/speed - i));
                 }
-                if( i === 2 * moveSize / speed)
-                isMove = false;
+                if( i >= 2 * moveSize / speed )
+                    isMove = false;
             }, updateTime * i)
             timeOutList.push(timeOut)
         }
+    */
     }
 
     const moveLeft = () => {
+        /*
+        console.log("Left")
         for(let i=0; i<2*moveSize/speed + 1; i++){
             let timeOut = setTimeout(() => {
                 if( i < moveSize / speed) {
+                    console.log("a"+i)
                     setLeft(initLeft - speed*i);
                 }
                 else {
+                    console.log("b"+i)
                     setLeft(initLeft - speed*(2 * moveSize/speed - i));
                 }
                 if( i === 2 * moveSize / speed)
-                isMove = false;
+                    isMove = false;
             }, updateTime * i)
             timeOutList.push(timeOut)
         }
+    */
+        setLeft(left-5);
     }
     // 렌더링
     return (
       <div>
-        <img id="FishingRod" src = {RodImg} className={classes.character} style={{left:left}} />
+        <img id="FishingRod" src = {RodImg} className={classes.character} style={{marginLeft: `${left}`+'px', marginTop: `${top}`+'px'}} />
       </div>
     )
 }
